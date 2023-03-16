ea4edcf2ff2012b2c3d473d185da13ce148d0e21
Subhajit Majumder <subhajit.winer@gmail.com>
2023-03-16 23:03:50 +0530

    option not needed thats why its removed

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,5 +1,5 @@
 import { UserModel } from "../models/user.model";
-import { Sequelize, Options } from "sequelize";
+import { Sequelize } from "sequelize";
 import { ProductModel } from '../models/product.model';
 import * as dotenv from 'dotenv';
 
